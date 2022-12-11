2d6927ba1a09ccf5d30b754499a2d3c1b188aa17
Ascyam <83872176+Ascyam@users.noreply.github.com>
2022-12-12 00:49:42 +0100

    Update leTaquin.js

--- leTaquin.js
+++ leTaquin.js
@@ -3,24 +3,45 @@ for (var i = 0; i<16;i++) {
     TableI[i]=i;
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function shuffle(tab) {
+  for (var i = tab.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var k= tab[i];
+    tab[i]= tab[j];
+    tab[j]= k;
+  }
 }
 
-function get_random (list) {
-    return list[Math.floor((Math.random()*list.length))];
+function Deplacement (number) {
+  let T;
+  switch (0){
+    case TableI[number-1]:
+      T=TableI[number];
+      TableI[number]=TableI[number-1];
+      TableI[number-1]=T;
+      break;
+    case TableI[number+1]:
+      T=TableI[number];
+      TableI[number]=TableI[number+1];
+      TableI[number+1]=T;
+      break;
+    case TableI[number+4]:
+      T=TableI[number];
+      TableI[number]=TableI[number+4];
+      TableI[number+4]=T;
+      break;
+    case TableI[number-4]:
+      T=TableI[number];
+      TableI[number]=TableI[number-4];
+      TableI[number-4]=T;
+      break;
+    default:
+      console.log('Clique au mauvais endroit');
   }
+}
 
-  function shuffle(tab) {
-    for (var i = tab.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var k= tab[i];
-      tab[i]= tab[j];
-      tab[j]= k;
-    }
-  }
-shuffle(TableI);
-for (let i = 0; i <16; i++) {
-    console.log(TableI[i]);
+function Moving (number) {
+  Deplacement(number);
+  console.log(TableI[number]);
 }
 
